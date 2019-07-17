6026588947de7d5efbf9ad21a72e9839e0b1dbcd
Alex L. Deweert <alexander.deweert@gmail.com>
2019-07-16 23:08:11 -0700

    Create site.php

---
+++ site.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action = "site.php" method = "post" enctype = "multipart/form-data">
+        <input type="file" name="file" id="file"><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+        function displayImage() {
+            $img = file_get_contents($_FILES["file"]["tmp_name"]);
+            $filename = $_FILES["file"]["name"];
+            file_put_contents("./$filename",$img);
+            echo("<div id = \"divbg\" style = \"background-image:url('$filename')\">a</div>");
+        }
+        
+        if( isset($_POST["submit"])) {
+            displayImage();
+        }
+    ?>
+</body>
+</html>
